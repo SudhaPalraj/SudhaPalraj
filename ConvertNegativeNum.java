d6f2d340adce1069c7aa2f719ddaf8554599ae8b
SudhaPalraj <93433360+SudhaPalraj@users.noreply.github.com>
2021-10-31 17:55:07 +0530

    Add files via upload

---
+++ ConvertNegativeNum.java
@@ -0,0 +1,31 @@
+package week1day1;
+
+public class ConvertNegativeNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * 1. Initialize an integer with a negative number like, int number = -40; 2.
+		 * Check if the number is a negative number Hint : any number that is lesser
+		 * than zero is a negative number 3. If so, convert the number to a positive
+		 * numer
+		 * 
+		 * 4. Print as below "The number -40 is converted to 40"
+		 */
+		
+		int num=-40;
+		
+		if (num<0) {
+			
+			num=40;
+			
+			System.out.println("The number -40 is converted to 40");
+		}
+		
+		
+		
+		
+	}
+
+}
